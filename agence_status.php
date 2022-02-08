cb184aa2a01f827b6675ac6077f526065f5fd568
kam <kamaltal123@gmail.com>
2022-02-08 20:09:38 +0100

    Fix inner join query in demande/user table

--- agence_status.php
+++ agence_status.php
@@ -19,13 +19,63 @@ include("./templates/base_top.php");
 
 <!-- Start section-->
 <div class="container_status">
+
+    <!-- Show all demande records. -->
     <div class="demand_status">
         <h2>Demands List</h2>
         <table>
+            <tr>
+                <th>Id Demand</th>
+                <th>Matricul</th>
+                <th>User</th>
+                <th>Durration</th>
+            </tr>
+            <tbody>
+                <?php
+                $query1 = "select demande.Id_demande, demande.Matr, utilisateur.Name, demande.Duree from demande INNER JOIN utilisateur ON demande.Id_utisateur=utilisateur.Id_U;";
+                $result1 = mysqli_query($connect, $query1);
 
-            hello
+                if (mysqli_num_rows($result1) > 0) {
+                    while ($row = mysqli_fetch_assoc($result1)) {
+                        echo "<tr> <td>" . $row['Id_demande'] .  "</td> <td>" . $row['Matr'] .  "</td> <td>" . $row['Name'] .  "</td> <td>" . $row['Duree'] . " Jrs</td></tr>";
+                    }
+                }
+                mysqli_free_result($result1);
+
+                ?>
+            </tbody>
         </table>
     </div>
+
+    <!-- Show all Users records. -->
+    <div class="Users_status">
+        <h2>Users List</h2>
+        <table>
+            <tr>
+                <th>User ID</th>
+                <th>User Name</th>
+                <th>User Phone</th>
+            </tr>
+            <tbody>
+
+                <?php
+                $query2 = "SELECT * FROM utilisateur";
+                $result2 = mysqli_query($connect, $query2);
+
+                if (mysqli_num_rows($result2) > 0) {
+                    while ($row = mysqli_fetch_assoc($result2)) {
+                        echo "<tr> <td>" . $row['Id_U'] . "</td><td>" . $row['Name'] . "</td><td>" . $row['Tele'] . "</td> </tr>";
+                    }
+                }
+                mysqli_free_result($result2);
+
+
+                ?>
+            </tbody>
+        </table>
+    </div>
+
+    <!-- Show all cars records. -->
     <div class="Cars_status">
         <h2>Cars List</h2>
         <?php
@@ -35,10 +85,10 @@ include("./templates/base_top.php");
         <table>
             <!-- <th> -->
             <tr>
-                <td>Matricule</td>
-                <td>Couleur</td>
-                <td>Type</td>
-                <td>Manuel</td>
+                <th>Matricule</th>
+                <th>Couleur</th>
+                <th>Type</th>
+                <th>Manuel</th>
             </tr>
             <!-- </th> -->
             <tbody>
--- index.php
+++ index.php
@@ -3,7 +3,27 @@ include("./templates/base_top.php");
 ?>
 <!-- Start Section -->
 
-<h1>Hello World</h1>
+<div class="h1_home">
+    <h1 class="header">Welcom Kam</h1>
+
+</div>
+<section>
+    <div class="container_home">
+        <!-- elemets of home page -->
+        <div class="box">
+            <img src="./style/images/user_icon.png" alt="">
+            <a class="home_link" href="./agence_adduser.php">Add New User</a>
+        </div>
+        <div class="box">
+            <img src="./style/images/demand_icon.png" alt="">
+            <a class="home_link" href="./agence_adduser.php">Create Demand</a>
+        </div>
+        <div class="box">
+            <img src="./style/images/car_icon.png" alt="">
+            <a class="home_link" href="./agence_addcar.php">Add New car</a>
+        </div>
+    </div>
+</section>
 
 <!-- End Section -->
 <?php
